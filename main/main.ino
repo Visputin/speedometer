1340b69edce1e65437d483d9a0e1253de32d0e13
visputin <lasaroffs@gmail.com>
2025-09-23 16:36:41 +0300

    display working, print centering helper added

---
+++ main/main.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Helper function to center prints
+void centerPrint(Adafruit_GFX &gfx, const char *text) {
+  int16_t x1, y1;
+  uint16_t w, h;
+  gfx.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+
+  int16_t x = (gfx.width() - w) / 2 - x1;
+  int16_t y = (gfx.height() - h) / 2 - y1;
+
+  gfx.setCursor(x, y);
+  gfx.print(text);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setFont();  
+  display.setTextColor(WHITE);
+
+  centerPrint(display, "Welcome!");
+  display.display();
+}
+
+void loop() {
+  
+}
